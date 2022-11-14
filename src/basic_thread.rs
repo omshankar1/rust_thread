39a3e7dee1468e0ae4660d699b405248cf59345c
Shankar Nagarajan <shankar.nagarajan@firstwavecloud.com>
2022-11-14 14:35:51 +1100

    fn closure asserts

--- src/basic_thread.rs
+++ src/basic_thread.rs
@@ -65,9 +65,10 @@ pub fn iterator_demo() {
     println!("Iterator result: {:?}", v1);
 }
 
-pub fn fnptr_fnclosure() {
+pub fn fnptr() {
+    use std::f64::consts::PI;
     fn area(r: i32) -> f64 {
-        (r * r) as f64 * std::f64::consts::PI
+        (r * r) as f64 * PI
     }
     /// Function Pointer
     let area_fnptr = area;
@@ -75,11 +76,52 @@ pub fn fnptr_fnclosure() {
 
     let v = (0..=10).collect::<Vec<_>>(); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     let area = v.iter().map(|r| area_fnptr);
+}
 
-    /// Closure with env capture 'PI'
-    let capture = std::f64::consts::PI;
-    let area_closure = |r: i32| (r * r) as f64 * capture;
-    println!("area closure: {}", area_closure(1));
+pub fn fnclosure() {
+    use std::f64::consts::PI;
+    //////////////////////////////////////////////////////////
+    /// Area using Closure with env capture 'PI'
 
-    let area = v.iter().map(|r| area_closure);
+    /// Fn Trait
+    let capture_height = 2; // Passed in as a capture
+    let area_closure1 = |r: i32| (r * r * capture_height) as f64 * PI;
+    println!("area closure: {}", area_closure1(1));
+    assert_fn(area_closure1);
+
+    let v = (0..=10).collect::<Vec<_>>(); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    let area = v.iter().map(|r| area_closure1);
+
+    //////////////////////////////////////////////////////////
+    /// FnMut Trait
+    let mut capture_height = 2; // Passed in as a capture
+    let mut area_closure2 = |r: i32| {
+        capture_height = capture_height + 1;
+        (r * r * capture_height) as f64 * PI
+    };
+    println!("area closure: {}", area_closure2(1));
+    assert_fnmut(area_closure2);
+
+    //////////////////////////////////////////////////////////
+    /// FnOnce Trait
+    let capture_height = 2; // Passed in as a capture
+    let capture_height = 2; // Passed in as a capture
+    let area_closure3 = move || {
+        let capture_height = capture_height + 1;
+        let r = 9;
+        (r * r * capture_height) as f64 * PI
+    };
+    // Closure to rust thread needs to be FnOnce
+    thread::spawn(area_closure3);
+}
+
+pub fn assert_fn<F>(f: F)
+where
+    F: Fn(i32) -> f64,
+{
+}
+pub fn assert_fnmut<F>(f: F)
+where
+    F: FnMut(i32) -> f64,
+{
 }
